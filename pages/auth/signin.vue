56e5c4dec7be7612d0a58671a674c48fb1aa3464
Elijah-afk-c <christianelijahbaltazar31@gmail.com>
2025-06-20 17:47:46 +0800

    added Login page

---
+++ pages/auth/signin.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class = "d-flex align-center justify-center" style="height: 100vh;">
+    
+
+    <v-card
+      class="mx-auto pa-12 pb-8"
+      elevation="1"
+      max-width="500"
+      rounded="lg"
+    >
+      <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+
+      <v-text-field
+        density="compact"
+        placeholder="Email address"
+        prepend-inner-icon="mdi-email-outline"
+        variant="outlined"
+      ></v-text-field>
+
+      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+        Password
+
+      
+      </div>
+
+      <v-text-field
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        placeholder="Enter your password"
+        prepend-inner-icon="mdi-lock-outline"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+      ></v-text-field>
+
+      <v-card
+        class="mb-12"
+        color="surface-variant"
+        variant="tonal"
+      >
+        
+      </v-card>
+
+      <v-btn
+        class="mb-8"
+        color="blue"
+        size="large"
+        variant="tonal"
+        block
+      >
+        Log In
+      </v-btn>
+
+      <v-card-text class="text-center">
+        <a
+          class="text-blue text-decoration-none"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <v-icon icon="mdi-chevron-right"></v-icon>
+        </a>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+<script setup>
+  import { ref } from 'vue'
+
+  const visible = ref(false)
+</script>
+<script>
+export default {
+  layout: "auth"
+}
+</script>
